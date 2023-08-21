f338626d0fdfed1de1595df613feb529b2f04257
Henrique da Costa Araujo <henrique.arauj0@outlook.com>
2023-08-21 15:27:48 -0300

    Add files via upload

---
+++ assets/assets/js/send-form.js
@@ -0,0 +1,64 @@
+class formSubmit {
+    constructor() {
+        this.settings = settings;
+        this.form = document.querySelector(this.settings.form);
+        this.formButton = document.querySelector(this.settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    }
+
+    getFormObject(){
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+
+    onSubmission(event){
+        event.preventDefault();
+        event.target.disabled = true;
+        event.target.innerText = "Mensagem enviada"
+    }
+
+    async sendForm(event) {
+        try {
+            this.onSubmission(event);
+            await fetch(this.url, {
+                metyhod: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    accept: "application/json"
+                },
+                body: JSON.stringify(this.getFormObject()),
+            });
+            this.displaySuccess();
+        } catch (error) {
+            this.displayError();
+            throw new Error(error);
+        }
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListener("click", () => this.sendForm);
+        return this;
+    }
+}
+
+const formSubmit = new formSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!<h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem<h1>",
+});
+formSubmit.init();
